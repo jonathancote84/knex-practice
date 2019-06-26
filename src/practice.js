0ca83c571a433b4ca9d03534dbb85bb151dcb9c4
Jonathan Cote <jonathancote92@gmail.com>
2019-06-26 12:30:35 -0700

    checkpoint 13

---
+++ src/practice.js
@@ -0,0 +1,69 @@
+require('dotenv').config()
+const knex = require('knex')
+
+const knexInstance = knex({
+    client: 'pg',
+    connection: process.env.DB_URL,
+})
+
+function searchByProductName(searchTerm) {
+    knexInstance
+    .from('amazong_products')
+    .select('product_id', 'name', 'price', 'category')
+    .where('name', 'ILIKE', `%${searchTerm}%`)
+    .then(result => {
+        console.log(result)
+    })    
+}
+
+// searchByProductName('holo')
+
+function paginateProducts(page) {
+    const productPerPage = 10
+    const offset = productPerPage * (page -1)
+    knexInstance
+      .select('product_id', 'name', 'price', 'category')
+      .from('amazong_products')
+      .limit(productPerPage)
+      .offset(offset)
+      .then(result => {
+          console.log(result)
+      })
+}
+
+// paginateProducts(2)
+
+function getProductWithImages() {
+    knexInstance
+      .select('product_id', 'name', 'price', 'category')
+      .from('amazong_products')
+      .whereNotNull('image')
+      .then(result => {
+          console.log(result)
+      })
+}
+
+// getProductWithImages()
+
+function mostPopularVideosForDays(days) {
+    knexInstance
+      .select('video_name', 'region')
+      .count('date_viewed AS views')
+      .where(
+          'date_viewed',
+          '>',
+          knexInstance.raw(`now() - '?? days'::INTERVAL`, days) 
+      )
+      .from('whopipe_video_views')
+      .groupBy('video_name', 'region')
+      .orderBy([
+          { column: 'region', order: 'ASC' },
+          { column: 'views', order: 'DESC' },
+      ])
+      .then(result => {
+          console.log(result)
+      })
+}
+
+// mostPopularVideosForDays(30)
+
